0d30d7eb8ac5eff0ba2f2d4708820e056616a5e1
HaiAuHapHoi <135623930+JvIadEye@users.noreply.github.com>
2025-10-14 16:50:04 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,13 +5,13 @@ def buildApp(){
 
 def pushImage(){
     echo 'Building Image ...'
-    sh "docker build -t 172.16.2.74:9006/dockerhosted-repo:${BUILD_NUMBER} ."
+    sh "docker build -t 172.16.16.74:9006/dockerhosted-repo:${BUILD_NUMBER} ."
 
     echo 'Pushing image to docker hosted rerpository on Nexus'
 
-    withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'PSW', usernameVariable: 'USER')]){
-        sh "docker login 172.16.2.74:9006 -u ${USER} -p ${PSW}"
-        sh "docker push 172.16.2.74:9006/dockerhosted-repo:${BUILD_NUMBER}"
+    withCredentials([usernamePassword(credentialsId: 'NexusRepository', passwordVariable: 'PSW', usernameVariable: 'USER')]){
+        sh "docker login 172.16.16.74:9006 -u ${USER} -p ${PSW}"
+        sh "docker push 172.16.16.74:9006/dockerhosted-repo:${BUILD_NUMBER}"
     }
 }
 
@@ -24,7 +24,7 @@ def deployApp(){
 }
 
 def commitChanges(){
-    withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'PSW', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'Github', passwordVariable: 'PSW', usernameVariable: 'USER')]) {
         sh 'git config --global user.name "jenkins"'
         sh 'git config --global user.email "jenkins@gmail.com"'
         sh "git remote set-url origin https://${USER}:${PSW}@github.com/JvIadEye/Jenkins-Nexus-CICD"
